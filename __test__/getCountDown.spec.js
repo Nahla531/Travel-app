188a5492bb17317ced26982c1216cb85ff1a6dec
Nahla531 <nhlaalekesh@gmail.com>
2021-03-10 18:09:28 +0200

    added test

---
+++ __test__/getCountDown.spec.js
@@ -0,0 +1,6 @@
+import { getCountDown } from "../src/client/js/app"
+describe("Testing the submit functionality", () => {
+    test("Testing the getCountDown() function", () => {
+        expect(getCountDown).toBeDefined();
+    })
+});
