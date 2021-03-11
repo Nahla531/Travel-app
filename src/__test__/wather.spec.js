60f7b0d9847dc7bab3f6a85e50ff76e77069608c
Nahla531 <nhlaalekesh@gmail.com>
2021-03-11 14:28:07 +0200

    solved babell stuff

---
+++ src/__test__/wather.spec.js
@@ -0,0 +1,5 @@
+import { Weather } from "../client/js/app"
+
+test("Testing the weather function", () => {
+    expect(typeof Weather).toBe('function');
+})
--- src/__test__/wather.test.js
+++
@@ -1,6 +0,0 @@
-import { handlefunc } from "../client/js/app"
-describe("Testing the handlefunc if it is function", () => {
-    test("Testing the getCountDown() function", () => {
-        expect(typeof handlefunc).toBe('function');
-    })
-});
